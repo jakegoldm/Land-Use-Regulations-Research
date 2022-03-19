67257750551e0a99b9cfebb0ac493aad348011c5
Jake Goldman <92948495+jakegoldm@users.noreply.github.com>
2022-03-19 13:18:07 -0400

    Update housing_supply_adjustments.py

--- housing_supply_adjustments.py
+++ housing_supply_adjustments.py
@@ -9,13 +9,16 @@ print(df2010)
 df1980 = pd.read_csv("/Users/USER/Desktop/Research Spreadsheets/Census1990Virginia.csv", sep=",",
                     index_col="Geography")
 print(df1980)
+
 df = pd.merge(df2000, df2010, how="inner", on="Geography")
 df = pd.merge(df, df1980, how="inner", on="Geography")
 df = df.reindex(sorted(df.columns), axis=1)
 print(df)
+
 df.to_csv("/Users/USER/Desktop/Research Spreadsheets/Housing1980-2018.csv", sep=",")
 df2 = df.interpolate(method="linear", axis=1)
 df2.to_csv("/Users/USER/Desktop/Research Spreadsheets/HousingLinearInterpolation.csv", sep=",")
+
 adjustments=[0.5,0.75,1,1.25,1.5]
 for adjustment in adjustments:
    df3 = df2.copy(deep=True)
